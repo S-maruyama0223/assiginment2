9c8f1119eea8394b0c87983b2db3fe822c0a726a
Shotaro Maruyama <testSample@aaa.com>
2020-10-22 19:27:21 +0900

    修正後

--- assignment2/ViewController.swift
+++ assignment2/ViewController.swift
@@ -6,49 +6,58 @@
 //
 
 import UIKit
+protocol ArithmeticOperation {
+    var function: (Double, Double) -> Double { get }
+}
+
+struct Addition: ArithmeticOperation {
+    let function: (Double, Double) -> Double = (+)
+}
+
+struct Subtraction: ArithmeticOperation {
+    let function: (Double, Double) -> Double = (-)
+}
+
+struct Multiplication: ArithmeticOperation {
+    let function: (Double, Double) -> Double = (*)
+}
+
+struct Division: ArithmeticOperation {
+    let function: (Double, Double) -> Double = (/)
+}
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak private var leftNumField: UITextField!
     @IBOutlet weak private var rightNumField: UITextField!
     @IBOutlet weak private var fourArithmeticSegment: UISegmentedControl!
     @IBOutlet weak private var resultLabel: UILabel!
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
     @IBAction func tapSumButton(_ sender: Any) {
-        let calcResult = calculation(fourArithmeticSegment.selectedSegmentIndex)
+        let arithmeticOperation: ArithmeticOperation
+        switch fourArithmeticSegment.selectedSegmentIndex {
+            case 0: arithmeticOperation = Addition()
+            case 1: arithmeticOperation = Subtraction()
+            case 2: arithmeticOperation = Multiplication()
+            case 3: arithmeticOperation = Division()
+            default: fatalError("selectedSegmentIndex is invalid.")
+        }
 
-        
+        let calcResult = calculation(arithmeticOperation: arithmeticOperation)
         resultLabel.text = calcResult
     }
-    
-    func calculation(_ operator:Int)-> String{
-        var calcResult:Double
+
+    func calculation(arithmeticOperation: ArithmeticOperation)-> String {
         let leftNum = Double(leftNumField?.text ?? "0.0") ?? 0.0
         let rightNum = Double(rightNumField?.text ?? "0.0") ?? 0.0
-        switch `operator`{
-        case 0:
-            calcResult = leftNum + rightNum
-        case 1:
-            calcResult = leftNum - rightNum
-        case 2:
-            calcResult = leftNum * rightNum
-        default:
-            if(rightNum == 0.0){
+
+        if arithmeticOperation is Division {
+            guard rightNum != 0.0 else {
                 return "0で除算しました"
-            }else{
-                calcResult = leftNum / rightNum
             }
         }
-        return String(calcResult)
+
+        return String(arithmeticOperation.function(leftNum, rightNum))
     }
-    
-    
-    
 }
 
